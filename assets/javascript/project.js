524abb98541b7cb54ee3bfa954c5fea0889d455c
Lucas Merat <lucas.merat@gmail.com>
2019-02-07 10:33:39 -0600

    Successful call to API to pull long and lat

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -4,3 +4,20 @@
 let query = "Austin"
 let URL = "https://developers.zomato.com/api/v2.1/locations?query=" + query;
 let key = "c7026f9c7b7d563d6a029354b3f03a9a";
+
+function loadLongLat(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', URL, true);
+    xhr.setRequestHeader('user-key', key)
+
+    xhr.onload = function() {
+        if(this.status == 200){
+            let location = JSON.parse(this.responseText);
+            console.log(location)
+        }
+    }
+
+    xhr.send();
+}
+
+loadLongLat()

644599baf164194b0d25f490b27e5350b734d72d
Lucas Merat <lucas.merat@gmail.com>
2019-02-07 10:16:48 -0600

    Removed extra app.js file and added initial elements of zomato API call to project.js

---
+++ assets/javascript/project.js
@@ -0,0 +1,6 @@
+//Zomato API Call
+
+//Location query to recieve log and lat
+let query = "Austin"
+let URL = "https://developers.zomato.com/api/v2.1/locations?query=" + query;
+let key = "c7026f9c7b7d563d6a029354b3f03a9a";

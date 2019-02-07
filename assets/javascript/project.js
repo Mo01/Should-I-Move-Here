0a5d69e00a86bf3803496d134509f34bdcff1e76
Lucas Merat <lucas.merat@gmail.com>
2019-02-07 11:29:10 -0600

    Working pull for city info

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,7 +1,7 @@
 //Zomato API Call
 
 //Location query to recieve log and lat
-let query = "Austin"
+let query = "Sag Harbor"
 let URL = "https://developers.zomato.com/api/v2.1/locations?query=" + query;
 let key = "c7026f9c7b7d563d6a029354b3f03a9a";
 
@@ -13,10 +13,24 @@ function loadLongLat(){
     xhr.onload = function() {
         if(this.status == 200){
             let location = JSON.parse(this.responseText);
-            console.log(location)
-        }
-    }
+            let lat = location.location_suggestions[0].latitude;
+            let long = location.location_suggestions[0].longitude;
+            console.log(lat, long)
+            let URL = "https://developers.zomato.com/api/v2.1/geocode?lat=" + lat + "&lon=" + long
+            
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', URL, true);
+            xhr.setRequestHeader('user-key', key)
 
+            xhr.onload = function(){
+                    let cityInfo = JSON.parse(this.responseText);
+                    console.log(cityInfo.popularity)
+            }
+        
+            xhr.send();
+        }
+        
+    }
     xhr.send();
 }
 
